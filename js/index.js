fbb3d40b4c2e830d02551b9464804425a51e8934
Patricia Naomi <patricianaomiyama@gmail.com>
2024-02-18 15:16:30 -0300

    alterações animações

--- js/index.js
+++ js/index.js
@@ -1,10 +1,34 @@
+const sarita = document.querySelector('.sarita');
+const monster = document.querySelector('.monster');
+let isJumping = false;
 
+const jump = () => {
+    if (isJumping) return; //Pra Sarita não ficar pulando várias vezes
+    isJumping = true;
+    sarita.classList.add('jump');
+    sarita.src = '/assets/css/images/sarita_jump.gif';
 
-
-signo1.onchange = function() {
-    signoTitulo.innerText = this.value;
+    setTimeout(() => {
+        sarita.classList.remove('jump');
+        sarita.src = '/assets/css/images/sarita_walking.gif'; // Volta para imagem de caminhada
+        isJumping = false;
+    }, 700);
 }
 
-function textoSigno() {
-    
-}
+const loop = setInterval(() => {
+
+    const monsterPosition = monster.offsetLeft;
+    const saritaPosition = +window.getComputedStyle(sarita).bottom.replace('px', '');
+                            // o '+' tenta converter a String em número!
+
+    console.log(saritaPosition);
+
+    if(monsterPosition <= 60 && monsterPosition > 0 && saritaPosition < 60) {
+        sarita.src = '/assets/css/images/sarita_dead.gif' + '?a=' + Math.random();
+        monster.style.animationPlayState = 'paused';
+        clearInterval(loop);
+        monster.style.left = `${monsterPosition}px`;
+    }
+}, 10);
+
+document.addEventListener('keydown', jump);
